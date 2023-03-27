e4eee742a926b0d41f628573707c5800635d5d77
Didio Perdomo <didioarbey@gmail.com>
2023-03-27 16:25:48 -0500

    login apuntando ala base de datos

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,79 +1,78 @@
+
 const jwt = require('jsonwebtoken')
 const bcryptjs = require('bcryptjs')
 const conexion = require('../database/db')
 const {promisify} = require('util')
 
-//procedimiento paa registrarnos
-exports.register = async (req,res)=>{
+//procedimiento para registrarnos
+exports.register = async (req, res)=>{    
     try {
         const name = req.body.name
         const user = req.body.user
         const pass = req.body.pass
-        //console.log(name + " - " + user + " - " + " - " + pass)
-        let passHash = await bcryptjs.hash(pass,8)
-        //console.log(passHash)
-        conexion.query('INSERT INTO users Set ?',{user:user,name:name,pass:passHash},(error,results)=>{
+        let passHash = await bcryptjs.hash(pass, 8)    
+        //console.log(passHash)   
+        conexion.query('INSERT INTO users SET ?', {user:user, name: name, pass:passHash}, (error, results)=>{
             if(error){console.log(error)}
             res.redirect('/')
         })
     } catch (error) {
         console.log(error)
-    }
+    }       
 }
 
-exports.login = async (req,res)=>{
+exports.login = async (req, res)=>{
     try {
         const user = req.body.user
-        const pass = req.body.pass
-        console.log(user + ' - ' + pass)
-        if (!user || !pass) {
+        const pass = req.body.pass        
+
+        if(!user || !pass ){
             res.render('login',{
                 alert:true,
                 alertTitle: "Advertencia",
-                alertMessage: "Ingresar un usuario y/o conraseña",
-                alertIcon: 'info',
+                alertMessage: "Ingrese un usuario y password",
+                alertIcon:'info',
                 showConfirmButton: true,
                 timer: false,
                 ruta: 'login'
             })
         }else{
-            conexion.query('SELECT * FROM users WHERE user =?', [user], async (error,results)=>{
-                if (results.length == 0 || !(await bcryptjs.compare(pass,results[0].pass))) {
-                    res.render('login',{
-                        alert:true,
-                        alertTitle: "Advertencia",
-                        alertMessage: "Ingresar un usuario y/o conraseña",
-                        alertIcon: 'info',
+            conexion.query('SELECT * FROM users WHERE user = ?', [user], async (error, results)=>{
+                if( results.length == 0 || ! (await bcryptjs.compare(pass, results[0].pass)) ){
+                    res.render('login', {
+                        alert: true,
+                        alertTitle: "Error",
+                        alertMessage: "Usuario y/o Password incorrectas",
+                        alertIcon:'error',
                         showConfirmButton: true,
                         timer: false,
-                        ruta: 'login'
+                        ruta: 'login'    
                     })
-                } else {
-                    //iniciode sesion ok
+                }else{
+                    //inicio de sesión OK
                     const id = results[0].id
-                    const token = jwt.sign({id:id}, process.env.JWT_SECRETO,{
+                    const token = jwt.sign({id:id}, process.env.JWT_SECRETO, {
                         expiresIn: process.env.JWT_TIEMPO_EXPIRA
                     })
-                    //generar token si expiracion
-                    //const token = jwt.sign(id:id),process.env.JWT_SECRETO)
-                    console.log("token: "+token+" para el usuario: "+user)
-                }
-                const cookiesOption = {
-                    expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-                    httpOnly: true
-                }
-                res.cookie('jwt',token,cookiesOption)
-                res.render('login',{
-                    alert: true,
-                    alertTitle: "Conexion Exitosa",
-                    alertMessage: "¡Login Correcto!",
-                    alertIcon: 'success',
-                    showConfirmButton: false,
-                    timer: 800,
-                    ruta: ''
-                })
+                    //generamos el token SIN fecha de expiracion
+                   //const token = jwt.sign({id: id}, process.env.JWT_SECRETO)
+                   console.log("TOKEN: "+token+" para el USUARIO : "+user)
 
-
+                   const cookiesOptions = {
+                        expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+                        httpOnly: true
+                   }
+                   res.cookie('jwt', token, cookiesOptions)
+                   res.render('login', {
+                        alert: true,
+                        alertTitle: "Conexión exitosa",
+                        alertMessage: "¡LOGIN CORRECTO!",
+                        alertIcon:'success',
+                        showConfirmButton: false,
+                        timer: 800,
+                        ruta: ''
+                   })
+                }
             })
         }
     } catch (error) {
