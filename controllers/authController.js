0305bf210173f4879c380b3ed4c9da194e1c3921
Didio Perdomo <didioarbey@gmail.com>
2023-03-26 23:04:18 -0500

    register con hash e insercion en la base de datos

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,8 +5,19 @@ const {promisify} = require('util')
 
 //procedimiento paa registrarnos
 exports.register = async (req,res)=>{
-    const name = req.body.name
-    const user = req.body.user
-    const pass = req.body.pass
-    console.log(name + " - " + user + " - " + " - " + pass)
+    try {
+        const name = req.body.name
+        const user = req.body.user
+        const pass = req.body.pass
+        //console.log(name + " - " + user + " - " + " - " + pass)
+        let passHash = await bcryptjs.hash(pass,8)
+        //console.log(passHash)
+        conexion.query('INSERT INTO users Set ?',{user:user,name:name,pass:passHash},(error,results)=>{
+            if(error){console.log(error)}
+            res.redirect('/')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
 }
