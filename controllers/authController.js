06253bd87455e78f117142a9ff881783ec3ba542
Didio Perdomo <didioarbey@gmail.com>
2023-03-27 22:03:40 -0500

    muetsra daniel

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,12 +5,12 @@ const conexion = require('../database/db')
 const {promisify} = require('util')
 
 //procedimiento para registrarnos
-exports.register = async (req, res)=>{    
+exports.register = async (req, res)=>{
     try {
         const name = req.body.name
         const user = req.body.user
         const pass = req.body.pass
-        let passHash = await bcryptjs.hash(pass, 8)    
+        let passHash = await bcryptjs.hash(pass, 8)
         //console.log(passHash)   
         conexion.query('INSERT INTO users SET ?', {user:user, name: name, pass:passHash}, (error, results)=>{
             if(error){console.log(error)}
